dbeaf22a6f4d1b858b8ef43c0eb89b910a13acc4
Sebastien Chiasson <sjchiass@gmail.com>
2023-10-18 18:09:17 -0400

    Move distorted ellipse into classes

--- catlib.py
+++ catlib.py
@@ -1,74 +1,5 @@
 from PIL import Image, ImageDraw
 
-# Define a disorted re-centered ellipse
-def distorted_ellipse(draw_obj, topleft_xy, bottomright_xy, center_xy, fill_rgb, quadrants=(True, True, True, True), stroke_width=(0, 0, 0, 0)):
-    # Given a corner and a center, determine the opposiste corner
-    # If you draw this on paper, the other corner is on the opposite side of the center
-    # that the original corner is
-    def opposite_corner(corner, center):
-        return (2*center[0] - corner[0], 2*center[1] - corner[1])
-    # Top left
-    if quadrants[0]:
-        draw_obj.pieslice(xy=[topleft_xy,
-                            opposite_corner(topleft_xy, center_xy)],
-                            start=180,
-                            end=270,
-                            fill=fill_rgb)
-        if stroke_width[0]:
-            draw_obj.arc(xy=[topleft_xy,
-                            opposite_corner(topleft_xy, center_xy)],
-                            start=180,
-                            end=270,
-                            fill="black",
-                            width=stroke_width[0])
-    # Top right
-    if quadrants[1]:
-        topright_xy = (bottomright_xy[0], topleft_xy[1])
-        topright_xy_opp = opposite_corner(topright_xy, center_xy)
-        draw_obj.pieslice(xy=[(topright_xy_opp[0], topright_xy[1]),
-                            (topright_xy[0], topright_xy_opp[1])],
-                            start=270,
-                            end=0,
-                            fill=fill_rgb)
-        if stroke_width[1]:
-            draw_obj.arc(xy=[(topright_xy_opp[0], topright_xy[1]),
-                            (topright_xy[0], topright_xy_opp[1])],
-                            start=270,
-                            end=0,
-                            fill="black",
-                            width=stroke_width[1])
-    # Bottom right
-    if quadrants[2]:
-        bottomright_xy_opp = opposite_corner(bottomright_xy, center_xy)
-        draw_obj.pieslice(xy=[(bottomright_xy_opp[0], bottomright_xy_opp[1]),
-                            (bottomright_xy[0], bottomright_xy[1])],
-                            start=0,
-                            end=90,
-                            fill=fill_rgb)
-        if stroke_width[2]:
-            draw_obj.arc(xy=[(bottomright_xy_opp[0], bottomright_xy_opp[1]),
-                            (bottomright_xy[0], bottomright_xy[1])],
-                            start=0,
-                            end=90,
-                            fill="black",
-                            width=stroke_width[2])
-    # Bottom left
-    if quadrants[3]:
-        bottomleft_xy = (topleft_xy[0], bottomright_xy[1])
-        bottomleft_xy_opp = opposite_corner(bottomleft_xy, center_xy)
-        draw_obj.pieslice(xy=[(bottomleft_xy[0], bottomleft_xy_opp[1]),
-                            (bottomleft_xy_opp[0], bottomleft_xy[1])],
-                            start=90,
-                            end=180,
-                            fill=fill_rgb)
-        if stroke_width[3]:
-            draw_obj.arc(xy=[(bottomleft_xy[0], bottomleft_xy_opp[1]),
-                            (bottomleft_xy_opp[0], bottomleft_xy[1])],
-                            start=90,
-                            end=180,
-                            fill="black",
-                            width=stroke_width[3])
-
 # Background
 class Head:
     def __init__(self, width=1, height=1, fill=(205, 127, 50)):
@@ -88,9 +19,78 @@ class Head:
                          (ox+self.w*1000, oy+self.h*1000)],
                      fill=self.f)
         return im
+    @staticmethod
+    # Define a disorted re-centered ellipse
+    def distorted_ellipse(draw_obj, topleft_xy, bottomright_xy, center_xy, fill_rgb, quadrants=(True, True, True, True), stroke_width=(0, 0, 0, 0)):
+        # Given a corner and a center, determine the opposiste corner
+        # If you draw this on paper, the other corner is on the opposite side of the center
+        # that the original corner is
+        def opposite_corner(corner, center):
+            return (2*center[0] - corner[0], 2*center[1] - corner[1])
+        # Top left
+        if quadrants[0]:
+            draw_obj.pieslice(xy=[topleft_xy,
+                                opposite_corner(topleft_xy, center_xy)],
+                                start=180,
+                                end=270,
+                                fill=fill_rgb)
+            if stroke_width[0]:
+                draw_obj.arc(xy=[topleft_xy,
+                                opposite_corner(topleft_xy, center_xy)],
+                                start=180,
+                                end=270,
+                                fill="black",
+                                width=stroke_width[0])
+        # Top right
+        if quadrants[1]:
+            topright_xy = (bottomright_xy[0], topleft_xy[1])
+            topright_xy_opp = opposite_corner(topright_xy, center_xy)
+            draw_obj.pieslice(xy=[(topright_xy_opp[0], topright_xy[1]),
+                                (topright_xy[0], topright_xy_opp[1])],
+                                start=270,
+                                end=0,
+                                fill=fill_rgb)
+            if stroke_width[1]:
+                draw_obj.arc(xy=[(topright_xy_opp[0], topright_xy[1]),
+                                (topright_xy[0], topright_xy_opp[1])],
+                                start=270,
+                                end=0,
+                                fill="black",
+                                width=stroke_width[1])
+        # Bottom right
+        if quadrants[2]:
+            bottomright_xy_opp = opposite_corner(bottomright_xy, center_xy)
+            draw_obj.pieslice(xy=[(bottomright_xy_opp[0], bottomright_xy_opp[1]),
+                                (bottomright_xy[0], bottomright_xy[1])],
+                                start=0,
+                                end=90,
+                                fill=fill_rgb)
+            if stroke_width[2]:
+                draw_obj.arc(xy=[(bottomright_xy_opp[0], bottomright_xy_opp[1]),
+                                (bottomright_xy[0], bottomright_xy[1])],
+                                start=0,
+                                end=90,
+                                fill="black",
+                                width=stroke_width[2])
+        # Bottom left
+        if quadrants[3]:
+            bottomleft_xy = (topleft_xy[0], bottomright_xy[1])
+            bottomleft_xy_opp = opposite_corner(bottomleft_xy, center_xy)
+            draw_obj.pieslice(xy=[(bottomleft_xy[0], bottomleft_xy_opp[1]),
+                                (bottomleft_xy_opp[0], bottomleft_xy[1])],
+                                start=90,
+                                end=180,
+                                fill=fill_rgb)
+            if stroke_width[3]:
+                draw_obj.arc(xy=[(bottomleft_xy[0], bottomleft_xy_opp[1]),
+                                (bottomleft_xy_opp[0], bottomleft_xy[1])],
+                                start=90,
+                                end=180,
+                                fill="black",
+                                width=stroke_width[3])
 
 # Mouth
-class Mouth:
+class Mouth(Head):
     def __init__(self, 
                  openness = 0.0, 
                  chin_x_offset = 0.0, chin_y_offset = 0.0, 
@@ -153,7 +153,7 @@ class Mouth:
         return im
 
 # Left cheek
-class LeftCheek:
+class LeftCheek(Head):
     def __init__(self, width=1, height=1, fill=(242, 140, 40)):
         self.w = width
         self.h = height
@@ -163,7 +163,7 @@ class LeftCheek:
         draw = ImageDraw.Draw(im)
         ox = 1250
         oy = 1900
-        distorted_ellipse(draw,
+        self.distorted_ellipse(draw,
                           (ox-self.w*500, oy-self.h*200),
                           (ox, oy+self.h*200),
                           (ox-self.w*300, oy),
@@ -172,7 +172,7 @@ class LeftCheek:
         return im
 
 # Right cheek
-class RightCheek:
+class RightCheek(Head):
     def __init__(self, width=1, height=1, fill=(242, 140, 40)):
         self.w = width
         self.h = height
@@ -182,7 +182,7 @@ class RightCheek:
         draw = ImageDraw.Draw(im)
         ox = 1250
         oy = 1900
-        distorted_ellipse(draw,
+        self.distorted_ellipse(draw,
                           (ox, oy-self.h*200),
                           (ox+self.w*500, oy+self.h*200),
                           (ox+self.w*300, oy),
@@ -191,7 +191,7 @@ class RightCheek:
         return im
 
 # Left ear
-class LeftEar:
+class LeftEar(Head):
     def __init__(self, width=1, height=1,
                  turn=0.0,
                  inner_fill=(184, 115, 51),
@@ -207,7 +207,7 @@ class LeftEar:
         # Draw the outer ear first
         o_ox = 450
         o_oy = 800
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (o_ox-self.w*200-self.t*100, o_oy-self.h*700+self.t*200),
                           (o_ox+self.w*600, o_oy+self.h*400),
                           (o_ox-self.t*100, o_oy+self.t*200),
@@ -217,7 +217,7 @@ class LeftEar:
         draw_ear = ImageDraw.Draw(ear)
         i_ox = 450
         i_oy = 600
-        distorted_ellipse(draw_ear,
+        self.distorted_ellipse(draw_ear,
                           (i_ox-(self.t+self.w)*100, i_oy-self.h*400+self.t*200),
                           (i_ox+(1-self.t)*self.w*300, i_oy+self.h*200+self.t*100),
                           (i_ox-self.t*150, i_oy),
@@ -228,7 +228,7 @@ class LeftEar:
         return composite
 
 # Right ear
-class RightEar:
+class RightEar(Head):
     def __init__(self, width=1, height=1,
                  turn=0.0,
                  inner_fill=(184, 115, 51),
@@ -245,7 +245,7 @@ class RightEar:
         # Draw the outer ear first
         o_ox = 2050
         o_oy = 800
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (o_ox-self.w*600, o_oy-self.h*700+self.t*200),
                           (o_ox+self.w*200+self.t*100, o_oy+self.h*400),
                           (o_ox+self.t*100, o_oy+self.t*200),
@@ -255,7 +255,7 @@ class RightEar:
         draw_ear = ImageDraw.Draw(ear)
         i_ox = 2050
         i_oy = 600
-        distorted_ellipse(draw_ear,
+        self.distorted_ellipse(draw_ear,
                           (i_ox-(1-self.t)*self.w*300, i_oy-self.h*400+self.t*200),
                           (i_ox+(self.t+self.w)*100, i_oy+self.h*200+self.t*100),
                           (i_ox+self.t*150, i_oy),
@@ -266,7 +266,7 @@ class RightEar:
         return composite
 
 # Nose
-class Nose:
+class Nose(Head):
     def __init__(self, width=1, height=1, upper_lip=True, fill=(160, 82, 45)):
         self.w = width
         self.h = height
@@ -286,7 +286,7 @@ class Nose:
         return im
 
 # Left eye
-class LeftEye:
+class LeftEye(Head):
     def __init__(self, 
                  squint = 0.25,
                  dilation = 0.1,
@@ -310,13 +310,13 @@ class LeftEye:
         mask = Image.new("L", im.size, "white")
         draw_mask = ImageDraw.Draw(mask)
         # Left eye mask
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (eox-self.ew*250, eoy-self.eh*200),
                           (eox+self.ew*250, eoy+self.eh*200),
                           (eox+50, eoy),
                           "black",
                           quadrants=(True, True, False, False))
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (eox-self.ew*250, eoy-self.eh*200),
                           (eox+self.ew*250, eoy+self.eh*200),
                           (eox-50, eoy),
@@ -334,7 +334,7 @@ class LeftEye:
         return composite
 
 # Right eye
-class RightEye:
+class RightEye(Head):
     def __init__(self,
                  squint = 0.25,
                  dilation = 0.1,
@@ -358,13 +358,13 @@ class RightEye:
         mask = Image.new("L", im.size, "white")
         draw_mask = ImageDraw.Draw(mask)
         # Left eye mask
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (eox-self.ew*250, eoy-self.eh*200),
                           (eox+self.ew*250, eoy+self.eh*200),
                           (eox-50, eoy),
                           "black",
                           quadrants=(True, True, False, False))
-        distorted_ellipse(draw_mask,
+        self.distorted_ellipse(draw_mask,
                           (eox-self.ew*250, eoy-self.eh*200),
                           (eox+self.ew*250, eoy+self.eh*200),
                           (eox+50, eoy),
